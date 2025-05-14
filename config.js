cc40025529d4a6a7aef62ada226d4ac0c7043449
Gracious Thindwa <graciousthindwa8@gmail.com>
2025-05-14 15:17:45 +0200

    Changed my config settings

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
  import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
     const supabase = createClient(
-      'https://yujceoyrxuribztxfoli.supabase.co',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1amNlb3lyeHVyaWJ6dHhmb2xpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMTc5NTQsImV4cCI6MjA2Mjc5Mzk1NH0.WRublEE33JlqxKT4zlpQq_pME6QT6i3B5WhdFTjkhg0'
-    );
+      'https://plypcvnfheiqgrfegitc.supabase.co',
+       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBseXBjdm5maGVpcWdyZmVnaXRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMTgzMzYsImV4cCI6MjA2Mjc5NDMzNn0.qHKzpoiETnL894GkfKEhwSD1POwYRj_NEaAq_YhXDyA'
+        );
 
     window.supabase = supabase;
