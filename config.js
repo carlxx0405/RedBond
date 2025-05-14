f9aee1c5233af942588f4d56a17a68bb574a83c8
carlxx0405 <sinjanicarlpc@gmail.com>
2025-05-14 22:32:37 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,3 +4,5 @@ const supabase = createClient(
   'https://zzkumnjebnmrriimiadh.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6a3VtbmplYm5tcnJpaW1pYWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMTM5NzksImV4cCI6MjA2Mjc4OTk3OX0.pBcl-0E2wQspaUKdyGJ3ck8nNJ5RpJgMNAeXQJ9QlHE'
 );
+
+window.supabase = supabase;
