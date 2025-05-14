8f7d38e0bc06733c81360e54ffab2e1e0c51a3eb
carlxx0405 <sinjanicarlpc@gmail.com>
2025-05-14 15:38:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabase = createClient(
-  'https://yujceoyrxuribztxfoli.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1amNlb3lyeHVyaWJ6dHhmb2xpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMTc5NTQsImV4cCI6MjA2Mjc5Mzk1NH0.WRublEE33JlqxKT4zlpQq_pME6QT6i3B5WhdFTjkhg0'
+  'https://zzkumnjebnmrriimiadh.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6a3VtbmplYm5tcnJpaW1pYWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMTM5NzksImV4cCI6MjA2Mjc4OTk3OX0.pBcl-0E2wQspaUKdyGJ3ck8nNJ5RpJgMNAeXQJ9QlHE'
 );
 
 window.supabase = supabase;
