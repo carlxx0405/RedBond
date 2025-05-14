18bb037c7b822f16bcd94776b387a68ec4f76263
carlxx0405 <sinjanicarlpc@gmail.com>
2025-05-14 22:39:16 +0200

    Updated fetch for cards
    Admin dashboard

--- config.js
+++ config.js
@@ -4,3 +4,5 @@ const supabase = createClient(
   'https://yujceoyrxuribztxfoli.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl1amNlb3lyeHVyaWJ6dHhmb2xpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcyMTc5NTQsImV4cCI6MjA2Mjc5Mzk1NH0.WRublEE33JlqxKT4zlpQq_pME6QT6i3B5WhdFTjkhg0'
 );
+
+window.supabase = supabase;
